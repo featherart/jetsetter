3d6c89c4fe968b2ea38c45316453c4f1de2e0215
Feather Knee <featherknee@gmail.com>
2017-11-16 10:22:27 -0800

    Basic state version

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,44 +1,77 @@
-import React, { Component } from 'react';
-import uniqueId from 'lodash/uniqueId';
-import CountDown from './CountDown';
-import NewItem from './NewItem';
-import Items from './Items';
+import React, { Component } from "react";
+import uniqueId from "lodash/uniqueId";
+import CountDown from "./CountDown";
+import NewItem from "./NewItem";
+import Items from "./Items";
 
-import './Application.css';
+import "./Application.css";
 
 const defaultState = [
-  { value: 'Pants', id: uniqueId(), packed: false },
-  { value: 'Jacket', id: uniqueId(), packed: false },
-  { value: 'iPhone Charger', id: uniqueId(), packed: false },
-  { value: 'MacBook', id: uniqueId(), packed: false },
-  { value: 'Sleeping Pills', id: uniqueId(), packed: true },
-  { value: 'Underwear', id: uniqueId(), packed: false },
-  { value: 'Hat', id: uniqueId(), packed: false },
-  { value: 'T-Shirts', id: uniqueId(), packed: false },
-  { value: 'Belt', id: uniqueId(), packed: false },
-  { value: 'Passport', id: uniqueId(), packed: true },
-  { value: 'Sandwich', id: uniqueId(), packed: true },
+  { value: "Pants", id: uniqueId(), packed: false },
+  { value: "Jacket", id: uniqueId(), packed: false },
+  { value: "iPhone Charger", id: uniqueId(), packed: false },
+  { value: "MacBook", id: uniqueId(), packed: false },
+  { value: "Sleeping Pills", id: uniqueId(), packed: true },
+  { value: "Underwear", id: uniqueId(), packed: false },
+  { value: "Hat", id: uniqueId(), packed: false },
+  { value: "T-Shirts", id: uniqueId(), packed: false },
+  { value: "Belt", id: uniqueId(), packed: false },
+  { value: "Passport", id: uniqueId(), packed: true },
+  { value: "Sandwich", id: uniqueId(), packed: true }
 ];
 
 class Application extends Component {
   state = {
     // Set the initial state,
+    items: defaultState
   };
 
   // How are we going to manipualte the state?
   // Ideally, users are going to want to add, remove,
   // and check off items, right?
+  addItem = item => {
+    this.setState({ items: [item, ...this.state.items] });
+  };
+
+  removeItem = item => {
+    this.setState({
+      items: this.state.items.filter(other => other.id !== item.id)
+    });
+  };
+
+  toggleItem = itemToToggle => {
+    const items = this.state.items.map(item => {
+      if (item.id !== itemToToggle) return item;
+      return { ...itemToToggle, packed: !itemToToggle.packed };
+    });
+    this.setState({ items });
+  };
+
+  markAsPacked = item => {
+    const otherItems = this.state.items.filter(other => other.id !== item.id);
+    const updatedItem = { ...item, packed: !item.packed };
+    this.setState({ items: [updatedItem, ...otherItems] });
+  };
+
+  markAllAsUnpacked = () => {
+    const items = this.state.items.map(item => ({ ...item, packed: false }));
+    this.setState({ items });
+  };
 
   render() {
     // Get the items from state
-
+    const { items } = this.state;
+    const packedItems = items.filter(item => item.packed);
+    const unpackedItems = items.filter(item => !item.packed);
     return (
       <div className="Application">
-        <NewItem />
-        <CountDown />
-        <Items title="Unpacked Items" items={[]} />
-        <Items title="Packed Items" items={[]} />
-        <button className="button full-width">Mark All As Unpacked</button>
+        <NewItem onSubmit={this.addItem} />
+        <CountDown {...this.state} />
+        <Items title="Unpacked Items" items={unpackedItems} onCheckOff={this.markAsPacked} onRemove={this.removeItem} />
+        <Items title="Packed Items" items={packedItems} onCheckOff={this.markAsPacked} onRemove={this.removeItem} />
+        <button className="button full-width" onClick={this.markAllAsUnpacked}>
+          Mark All As Unpacked
+        </button>
       </div>
     );
   }
--- src/components/Items.js
+++ src/components/Items.js
@@ -5,28 +5,32 @@ import Filter from './Filter';
 class Items extends Component {
   state = {
     // What state does this component have?
+    searchTerm: ''
   };
 
-  updateSearchTerm = searchTerm => {};
+  updateSearchTerm = searchTerm => {
+    this.setState({searchTerm});
+  };
 
   render() {
-    const { title, items } = this.props;
+    const { title, items, onCheckOff, onRemove } = this.props;
+    const { searchTerm } = this.state;
     return (
       <section className="Items">
         <h2>
           {title} ({items.length})
         </h2>
-        <Filter searchTerm={''} onChange={this.updateSearchTerm} />
+        <Filter searchTerm={searchTerm} onChange={this.updateSearchTerm} />
         {items
           .filter(item =>
             // Hmm… this needs some work.
-            item.value.toLowerCase().includes(''.toLowerCase()),
+            item.value.toLowerCase().includes(searchTerm.toLowerCase()),
           )
           .map(item => (
             <Item
               key={item.id}
-              onCheckOff={() => {}}
-              onRemove={() => {}}
+              onCheckOff={() => onCheckOff(item)}
+              onRemove={() => onRemove(item)}
               item={item}
             />
           ))}
